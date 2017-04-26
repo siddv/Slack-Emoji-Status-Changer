0ad24a5133dd67750db1fc78152f5920caa47d62
Sam Wray <sam@wray.pro>
2017-04-26 17:55:01 +0100

    Add support for multiple tokens

--- index.js
+++ index.js
@@ -1,57 +1,49 @@
 require('isomorphic-fetch');
-require('dotenv').config();
+const tokens = require('./slack-api-tokens');
 
-const emojiList = require('./emojis.json').emojis;
-const token = process.env.SLACK_API_TOKEN;
 const timeout = 5000;
-
+const emojiList = require('./emoji-list');
 const numOfEmojis = emojiList.length;
-let counter = 0;
 
 /**
  * Returns a random emoji from the above list
  */
 const getRandomEmoji = function () {
   return emojiList[Math.floor(Math.random() * numOfEmojis)];
-}
+};
 
 /**
  * Sets the your slack status emoji until you stop the script with intervals defined in 'timeout' above
  */
-const setEmoji = function () {
+const setEmoji = function (token, counter) {
 
   let emoji = getRandomEmoji();
-  let url = 'https://slack.com/api/users.profile.set?token=' + token + '&profile=' + encodeURIComponent(JSON.stringify({'status_emoji': emoji}));
-  
+	let url = `https://slack.com/api/users.profile.set?token=${token}&profile=` + encodeURIComponent(JSON.stringify({
+    'status_emoji': emoji,
+    'status_text': `Emoji successfully changed ${counter} times.`
+  }));
+
   fetch(url).then((response) => {
+      if(response.status >= 400) {
+
+        throw new Error('On no. It broke.');
+
+      }
 
       return response.json();
 
     })
     .then((data) => {
-      
-      if (data.error) {
-        
-        console.log(`On no. It broke: ${data.error}\nTrying again...`);
-
-      } else {
-
-        counter++; 
-        console.log(`Emoji successfully changed ${counter} times. Current: ${emoji}`);
-
-      }
+      console.log(`Emoji successfully changed ${counter} times. Current: ${emoji}`);
+      counter++;
 
       setTimeout(() => {
-
-        setEmoji();
-
-      }, timeout);  
-
+        setEmoji(token, counter);
+      }, timeout);
     });
+};
 
-}
-
-/** 
+/**
  * Do the thing
  */
-setEmoji();
+tokens.forEach(token => setEmoji(token, 1));
